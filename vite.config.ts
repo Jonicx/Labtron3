e5b82da84bd193cb63c8c402e86ac6bebcb00df1
Jonicx <havugimana.jonathan4@gmail.com>
2025-02-14 12:51:44 +0200

    vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Labtron3/',
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html', // Explicitly define the input file
+    },
   },
 });
